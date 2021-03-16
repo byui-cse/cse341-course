84e16870fe8562e349af6d3236ae3d280f643c0a
727021 <1834544+727021@users.noreply.github.com>
2021-03-16 08:54:37 -0600

    New pr10 Solution

--- docs/lesson10/prove-solution/pr10-server.js
+++ docs/lesson10/prove-solution/pr10-server.js
@@ -12,11 +12,9 @@ router.post('/insertName', (req, res, next) => {
     // Typically you should do some sort of filtering and error checking. This is minimal, and makes sure we're not accepting empty values
     if (req.body.newName !== undefined) {
         const newName = req.body.newName
-        if (
-            dummyData.avengers.find(
-                element => element.name === req.body.newName
-            ) === undefined
-        ) { // Make our submissions somewhat unique.
+
+        // Make our submissions somewhat unique.
+        if (!dummyData.avengers.some(a => a.name === newName)) {
             dummyData.avengers.push({ name: newName }) // Push new object into the dummyData
             res.sendStatus(200)
         }
