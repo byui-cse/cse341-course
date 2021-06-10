6b75ef9563af2f4227660a2e261ac0cd37cd01a2
Nathan Birch <birchn@byui.edu>
2021-06-10 14:37:25 -0600

    adding clarity to lesson 2-6 prove assignments

--- docs/site/global.js
+++ docs/site/global.js
@@ -1,13 +1,14 @@
 // window.onload = function(){
-    // favicon
-    var link = top.document.createElement("link");
-    link.type = "image/x-icon";
-    link.rel = "shortcut icon";
-    var today = new Date();
-    var url = "https://byui-cse.github.io/cse341-course/site/";
-    link.href = url + "fav.png?";
-    if(today.getDay() == 6 || today.getDay() == 0) link.href = url + "happy-fav.png?";
-    top.document.getElementsByTagName("head")[0].appendChild(link);
+// favicon
+var link = top.document.createElement('link');
+link.type = 'image/x-icon';
+link.rel = 'shortcut icon';
+var today = new Date();
+var url = 'https://byui-cse.github.io/cse341-course/site/';
+link.href = url + 'fav.png?';
+if (today.getDay() == 6 || today.getDay() == 0)
+  link.href = url + 'happy-fav.png?';
+top.document.getElementsByTagName('head')[0].appendChild(link);
 // }
 
 // global constants
@@ -20,29 +21,33 @@ g.courseCodeName = g.courseCode + ' | ' + g.courseName;
 g.courseMaterials = g.courseCode + ' - Course Materials';
 
 // frequently used phrases
-g.prepare         = "Prepare: Reading Material";
-g.teach           = "Teach: Team Activity";
-g.ponder          = "Prove: Assignment";
-g.reading         = "For your technical reading this lesson, you will complete the following sections of the course: ";
-g.reading2        = g.reading.slice(0,-2) + ". Now that you are working on your team project for the remainder of the semester, you will notice that not all of the readings will apply to your team project. However, to continue your professional development and to be prepared for your prove assignments, you should be familiar with these concepts.";
-g.teachTime       = "Please spend at least 1 hour on this assignment before reviewing the solution.";
+g.prepare = 'Prepare: Reading Material';
+g.teach = 'Teach: Team Activity';
+g.ponder = 'Prove: Assignment';
+g.reading =
+  'For your technical reading this lesson, you will complete the following sections of the course: ';
+g.reading2 =
+  g.reading.slice(0, -2) +
+  '. Now that you are working on your team project for the remainder of the semester, you will notice that not all of the readings will apply to your team project. However, to continue your professional development and to be prepared for your prove assignments, you should be familiar with these concepts.';
+g.teachTime =
+  'Please spend at least 1 hour on this assignment before reviewing the solution.';
 g.teachInstructor = "Instructor's Solution";
 g.teachSolution =
-    "As a part of this team activity, you are expected to look over a solution from the instructor, to compare your approach to that one. One of the questions on the I-Learn submission will ask you to provide insights from this comparison. <br><br> Please DO NOT open the solution until you have worked through this activity as a team for the one hour period. At the end of the hour, if you are still struggling with some of the core requirements, you are welcome to view the instructor's solution and use it to help you complete your own code. Even if you use the instructor's code to help you, you are welcome to report that you finished the core requirements, if you code them up yourself.";
+  "As a part of this team activity, you are expected to look over a solution from the instructor, to compare your approach to that one. One of the questions on the I-Learn submission will ask you to provide insights from this comparison. <br><br> Please DO NOT open the solution until you have worked through this activity as a team for the one hour period. At the end of the hour, if you are still struggling with some of the core requirements, you are welcome to view the instructor's solution and use it to help you complete your own code. Even if you use the instructor's code to help you, you are welcome to report that you finished the core requirements, if you code them up yourself.";
 g.proveSolution =
-    "As a part of this prove assignment, you are expected to look over a solution from the instructor, to compare your approach to that one. <br><br> Please DO NOT open the solution until you have spent at least one hour working on this assignment. After you have spent at least an hour working on this assignment, you are welcome to view the instructor's solution and use it to help you complete your own code. Even if you use the instructor's code to help you, you can still recieve full credit for this assignment, if you code it yourself (please don't copy/paste any of the instructor code)."
+  "As a part of this prove assignment, you are expected to look over a solution from the instructor, to compare your approach to that one. <br><br> Please DO NOT open the solution until you have spent at least one hour working on this assignment. After you have spent at least an hour working on this assignment, you are welcome to view the instructor's solution and use it to help you complete your own code. Even if you use the instructor's code to help you, you can still recieve full credit for this assignment, if you code it yourself (please don't copy/paste any of the instructor code).";
 g.teachSubmit =
-    'When you have finished this activity, please fill out the assessment in I-Learn. You are welcome to complete any additional parts of this activity by yourself or with others after your meeting before submitting the assessment.';
+  'When you have finished this activity, please fill out the assessment in I-Learn. You are welcome to complete any additional parts of this activity by yourself or with others after your meeting before submitting the assessment.';
 g.ponderSubmit =
-    '<h2>Submission</h2>' +
-    '<ol>' +
-    '<li><p>Ensure that your latest code is pushed to GitHub and Heroku.</p></li>' +
-    '<li><p>Download this assignment\'s <a href="prove.txt" target="_blank">submission form</a>, fill it out, and submit it to I-Learn.</p></li>' +
-    '</ol>';
+  '<h2>Submission</h2>' +
+  '<ol>' +
+  '<li><p>Ensure that your latest code is pushed to GitHub and Heroku.</p></li>' +
+  '<li><p>Download this assignment\'s <a href="prove.txt" target="_blank">submission form</a>, fill it out, and submit it to I-Learn.</p></li>' +
+  '</ol>';
 g.project1req =
-    "Your assignment for this lesson will be 100% dedicated to your personal E-commerce project for this portion of the semester. To review project requirements please <a href='../projectStuff/prj1-requirements.html'>read here</a>.";
+  "Your assignment for this lesson will be 100% dedicated to your personal E-commerce project for this portion of the semester. The E-Commerce project spans lessons 2-6. Similar to last lesson, your assignment for this lesson will follow the Academind videos very closely. By simply completing the code demonstrated in the videos and pushing to Heroku, you'll be eligible for 93% on this assignment. To review project requirements please <a href='../projectStuff/prj1-requirements.html'>read here</a>.";
 g.project2req =
-    "Your assignment for this lesson will be 100% dedicated to your team project for this portion of the semester. To review project requirements please read here (<a href='../projectStuff/prj2/prj2-requirements.html'>full-stack</a>) (<a href='../projectStuff/prj2/wdd330_cse341/prj2-requirements.html'>backend</a>).";
+  "Your assignment for this lesson will be 100% dedicated to your team project for this portion of the semester. To review project requirements please read here (<a href='../projectStuff/prj2/prj2-requirements.html'>full-stack</a>) (<a href='../projectStuff/prj2/wdd330_cse341/prj2-requirements.html'>backend</a>).";
 
 // module numbers
 g.n01 = '01';
@@ -91,8 +96,8 @@ g.u13 = 'Lesson ' + g.n13 + ' - Wrap Up';
 
 // other repeated statements
 g.otherUnofficial =
-    'This is an unofficial listing of the course content. Please consult I-Learn for the official list of materials, including quizzes and due-dates.';
+  'This is an unofficial listing of the course content. Please consult I-Learn for the official list of materials, including quizzes and due-dates.';
 g.secondHalfProveMessage =
-    '<div class="tips">Special note:<p> While this prove assignment contains very valuable information, the concepts found in this prove are not required to be implemented into ' +
-    'the second project. These concepts are great stretch goals that can be added to the project and can help to reach 100%. This prove assignment can ' +
-    'be done in your groups or done individually. <b>Don\'t spend over an hour working on this on your own. Please use the teacher solution after an hour to finish the assignment.</b></p></div>';
+  '<div class="tips">Special note:<p> While this prove assignment contains very valuable information, the concepts found in this prove are not required to be implemented into ' +
+  'the second project. These concepts are great stretch goals that can be added to the project and can help to reach 100%. This prove assignment can ' +
+  "be done in your groups or done individually. <b>Don't spend over an hour working on this on your own. Please use the teacher solution after an hour to finish the assignment.</b></p></div>";
