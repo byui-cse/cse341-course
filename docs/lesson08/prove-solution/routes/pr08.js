51344db98fc626e2a608d5d0e375dc72da323998
= <birchn@byui.edu>
2021-07-02 08:34:25 -0600

    formatted all filesatted all files

--- docs/lesson08/prove-solution/routes/pr08.js
+++ docs/lesson08/prove-solution/routes/pr08.js
@@ -3,6 +3,5 @@ const express = require('express');
 const router = express.Router();
 var jsonEngine = require('../../controllers/w08/pr08.js');
 
-router.get('/', jsonEngine.processJson)
-      .post('/', jsonEngine.getIndex)
+router.get('/', jsonEngine.processJson).post('/', jsonEngine.getIndex);
 module.exports = router;
